63693bd93809f09284c9ac254027806b85617c12
bulat_mukhutdinov <bulat.mukhutdinov@btsdigital.kz>
2018-11-30 12:01:18 +0600

    init

---
+++ app/src/androidTest/java/ru/bulat/mukhutdinov/bitrisetest/MainActivityTest.kt
@@ -0,0 +1,23 @@
+package ru.bulat.mukhutdinov.bitrisetest
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun simpleTest() {
+        onView(withText("Hello World!"))
+            .check(matches(isDisplayed()))
+    }
+}
---
+++ app/src/main/java/ru/bulat/mukhutdinov/bitrisetest/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.bulat.mukhutdinov.bitrisetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
